01d60fe05b9877a67aa08b1ac9a1da77a5c2d7a4
haribashi <antonpaskar2003@gmail.com>
2022-11-27 11:54:49 +0200

    add new app in this app add models and migrated

---
+++ Backend/listOfName/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Name(models.Model):
+    name = models.CharField(max_length=100, null=False)
+
+
+class Rank(models.Model):
+    name = models.ForeignKey('Name', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rankNumber = models.IntegerField()
